889bada96e3ef02cbf4c2656785e830697b9ebbd
Unknown <jessica.gilbert@softwire.com>
2018-10-30 12:03:44 +0000

    Created list of locations from json file

---
+++ src/main/java/Location.java
@@ -0,0 +1,9 @@
+public class Location {
+    public String x;
+    public String y;
+    public String id;
+
+    public String toString() {
+        return "x: " + x + "\n" + " y: " + y + "\n" + " id: " + id + "\n";
+    }
+}
---
+++ src/test/java/GasMonTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import sun.applet.Main;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GasMonTest {
+
+    @Test
+    public void mainTest() {
+
+    }
+}
